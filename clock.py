174dbe7abd754441a48ac3a8f0e91dbca55a8b35
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-10-16 23:03:07 -0400

    misc

--- clock.py
+++ clock.py
@@ -2,8 +2,8 @@ class Clock(object):
     def __init__(self, time):
         self.time = time
 
-#    def print_time(self, time):
-#       print(time)
+    def print_time(self, time):
+        print(time)
     
     def print_time(self):
         print(self.time)
