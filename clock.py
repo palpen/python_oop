762821f8af38918dfe536da6750e2761882260d7
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-10-16 22:45:07 -0400

    initial commit

---
+++ clock.py
@@ -0,0 +1,9 @@
+class Clock(object):
+    def __init__(self, time):
+        self.time = time
+
+#    def print_time(self, time):
+#       print(time)
+    
+    def print_time(self):
+        print(self.time)
